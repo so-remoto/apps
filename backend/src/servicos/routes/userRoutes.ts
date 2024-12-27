c8d714c74b6cb5ba72d48401b78520accc5be65b
tiago-kelis <tiagokelis2017@gmail.com>
2024-12-27 18:41:49 -0300

    Dev_Tiago -- atualizando appRoutes

--- backend/src/servicos/routes/userRoutes.ts
+++ backend/src/servicos/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { getUsers, createUser } from "../controllers/useController";
+import { getUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/useController'; // Ajuste o caminho conforme necessário
 
-const router: Router = Router();
+const router = Router();
 
-router.get('/users', getUsers);
-router.post('/users', createUser);
+router.get('/users', getUsers); // Rota para obter todos os usuários
+router.get('/users/:id', getUserById); // Rota para obter um usuário por ID
+router.post('/users', createUser); // Rota para criar um novo usuário
+router.put('/users/:id', updateUser); // Rota para atualizar um usuário por ID
+router.delete('/users/:id', deleteUser); // Rota para deletar um usuário por ID
 
 export default router;
